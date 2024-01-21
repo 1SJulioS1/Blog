6e3b2129db6448027f012233a110bfebd5e56c04
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2024-01-21 16:49:32 -0600

    admin check

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,6 @@ const ROLES_LIST = require("../config/roles_list");
 const verifyRoles = require("../middleware/verifyRoles");
 const { getAllAdmins } = require("../controllers/admin/adminController");
 
-router.route("/").get(getAllAdmins);
+router.route("/").get(verifyRoles(ROLES_LIST.Admin), getAllAdmins);
 
 module.exports = router;
