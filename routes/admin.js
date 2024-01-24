270a1046850472711c4c76a50140af6cb818092a
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2024-01-24 13:40:43 -0600

    minor code improvements

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const ROLES_LIST = require("../config/roles_list");
-const verifyRoles = require("../middleware/verifyRoles");
 const { getAllAdmins } = require("../controllers/admin/adminController");
 const {
   createUser,
   getUser,
   updateUser,
 } = require("../controllers/user/userController");
+const verifyAdministrator = require("../middleware/verifySelfOrAdministrator");
+
 router
   .route("/")
-  .get(verifyRoles(ROLES_LIST.Admin), getAllAdmins)
-  .post(verifyRoles(ROLES_LIST.Admin), createUser);
+  .get(verifyAdministrator, getAllAdmins)
+  .post(verifyAdministrator, createUser);
 
 router
   .route("/:id")
-  .get(verifyRoles(ROLES_LIST.Admin), getUser)
-  .put(verifyRoles(ROLES_LIST.Admin), updateUser);
+  .get(verifyAdministrator, getUser)
+  .put(verifyAdministrator, updateUser);
 
 module.exports = router;
--- routes/editor.js
+++ routes/editor.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const ROLES_LIST = require("../config/roles_list");
-const verifyRoles = require("../middleware/verifyRoles");
 const verifySelfOrAdministrator = require("../middleware/verifySelfOrAdministrator");
 const verifyAdministrator = require("../middleware/verifySelfOrAdministrator");
 const {
@@ -10,7 +8,7 @@ const {
   updateUser,
 } = require("../controllers/user/userController");
 
-router.route("/").post(verifyRoles(ROLES_LIST.Admin), createUser);
+router.route("/").post(verifyAdministrator, createUser);
 
 router
   .route("/:id")
